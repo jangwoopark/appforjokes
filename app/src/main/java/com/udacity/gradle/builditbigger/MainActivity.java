c59c9dba9f89a0a2c039df70756ccfe8147bb71c
Jang Woo Park <jangpark@g.ucla.edu>
2018-09-04 16:48:10 -0700

    Add files via upload
    upload app

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,43 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MainActivityFragment mainActivityFragment = new MainActivityFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .add(R.id.main_activity_fragment_container, mainActivityFragment, MainActivityFragment.class.getName())
+                .commit();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
